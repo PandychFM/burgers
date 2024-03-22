2fbd516bd91a59e18f7a8fafcaf3970a31bf4143
Woo <vitaotkidac@gmail.com>
2024-03-22 16:19:25 +0300

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,104 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'burgers';
+  currency = '$';
+  loaderShowed = true;
+  loader = true;
+
+  orderImgStyle: any;
+  mainImgStyle: any;
+
+  form = this.fb.group({
+    order: ['', Validators.required],
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+  });
+
+  productsData: any;
+
+  constructor(private fb: FormBuilder, private appService: AppService) {}
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.orderImgStyle = {
+      transform:
+        'translate(-' +
+        (e.clientX * 0.3) / 8 +
+        'px,-' +
+        (e.clientX * 0.3) / 8 +
+        'px)',
+    };
+    this.mainImgStyle = {
+      transform:
+        'translate(-' +
+        (e.clientX * 0.3) / 8 +
+        'px,-' +
+        (e.clientX * 0.3) / 8 +
+        'px)',
+    };
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.loaderShowed = false;
+    }, 3000);
+    setTimeout(() => {
+      this.loader = false;
+    }, 4000);
+    this.appService.getData().subscribe((data) => (this.productsData = data));
+  }
+
+  scrollTo(target: HTMLElement, burger?: any) {
+    target.scrollIntoView({ behavior: 'smooth' });
+    if (burger) {
+      this.form.patchValue({
+        order: burger.title + ' (' + burger.price + ' ' + this.currency + ')',
+      });
+    }
+  }
+
+  confirmOrder() {
+    if (this.form.valid) {
+      this.appService.sendOrder(this.form.value).subscribe({
+        next: (response: any) => {
+          alert(response.message);
+          this.form.reset();
+        },
+        error: (response) => {
+          alert(response.error.message);
+        },
+      });
+    }
+  }
+
+  changeCurrency() {
+    let newCurrency = '$';
+    let coefficient = 1;
+
+    if (this.currency === '$') {
+      newCurrency = '₽';
+      coefficient = 91;
+    } else if (this.currency === '₽') {
+      newCurrency = 'BYN';
+      coefficient = 3;
+    } else if (this.currency === 'BYN') {
+      newCurrency = '€';
+      coefficient = 0.9;
+    } else if (this.currency === '€') {
+      newCurrency = '¥';
+      coefficient = 6.9;
+    }
+    this.currency = newCurrency;
+
+    this.productsData.forEach((item: any) => {
+      item.price = +(item.basePrice * coefficient).toFixed(1);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
